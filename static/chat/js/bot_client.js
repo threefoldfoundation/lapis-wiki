84a70c410e003fe647b9e8d6eeb3fad258425df8
Ahmed T. Youssef <xmonader@gmail.com>
2019-04-23 16:03:33 +0200

    restore bot client with location and captcha

--- static/chat/js/bot_client.js
+++ static/chat/js/bot_client.js
@@ -1,17 +1,21 @@
-import {default as client} from '/static/weblibs/gedis/gedis_client.js';
+import {
+    default as client
+} from '/static/chat/js/gedis_client.js';
 let EXEC_OBJ = {
-    "namespace": "system",
+    "namespace": "default",
     "actor": "chatbot",
-    "header": {"response_type": "json"},
+    "header": {
+        "response_type": "json"
+    },
 }
 
-var stringContentGenerate = function (message, kwargs){
+
+var stringContentGenerate = function (message, kwargs) {
     let contents = ``
-    if(typeof kwargs['default'] == 'undefined'){
-        contents =  `<input type="text" class="form-control" id="value">`
-    }
-    else{
-        contents =  `<input type="text" class="form-control" id="value" value="${kwargs["default"]}"`
+    if (typeof kwargs['default'] == 'undefined') {
+        contents = `<input type="text" class="form-control" id="value">`
+    } else {
+        contents = `<input type="text" class="form-control" id="value" value="${kwargs["default"]}"`
     }
     return `
     <h4>${message}</h4>
@@ -20,7 +24,7 @@ var stringContentGenerate = function (message, kwargs){
     </div>`
 }
 
-var passwordContentGenerate = function (message, kwargs){
+var passwordContentGenerate = function (message, kwargs) {
     return `
     <h4>${message}</h4>
     <div class="form-group">
@@ -28,7 +32,7 @@ var passwordContentGenerate = function (message, kwargs){
     </div>`
 }
 
-var textContentGenerate = function (message, kwargs){
+var textContentGenerate = function (message, kwargs) {
     return `
     <h4>${message}</h4>
     <div class="form-group">
@@ -36,7 +40,7 @@ var textContentGenerate = function (message, kwargs){
     </div>`
 }
 
-var intContentGenerate = function (message, kwargs){
+var intContentGenerate = function (message, kwargs) {
     return `
     <h4>${message}</h4>
     <div class="form-group">
@@ -44,15 +48,79 @@ var intContentGenerate = function (message, kwargs){
     </div>`
 }
 
-var mdContentGenerate = function (message, kwargs){
-    let converter = new showdown.Converter({tables: true, tablesHeaderId: "table"});
+var captchaContentGenerate = function (message, captcha, label, kwargs) {
+    return `
+    <h4>${message}</h4>
+    <img src="data:image/png;base64,${captcha}"/>
+    <div class="form-group">
+        <input type="text" placeholder="Captcha" class="form-control" id="value">
+    </div>
+    <label class="captcha-error">${label}</label>`
+}
+
+var locationContentGenerate = function (message, label, kwargs) {
+    return `
+    <h4>${message}</h4>
+    <div class="form-group">
+        <input type="text" placeholder="Location" class="form-control" id="value" readonly>
+        <div id="mymap" class="mapdiv" style="width: 60%; height: 300px;">
+        <script>
+            function locationChoiceGenerate() {
+                let lat = 51.260197;
+                let lng = 4.402771;
+            
+                let mymap = L.map('mymap').setView([lat, lng], 4);
+            
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                    maxZoom: 18,
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    id: 'mapbox.streets'
+                }).addTo(mymap);
+            
+                let popup = L.popup();
+            
+                function onMapClick(e) {
+                    popup
+                        .setLatLng(e.latlng)
+                        .setContent("You clicked the map at " + e.latlng.toString())
+                        .openOn(mymap);
+                    sLat = e.latlng['lat'];
+                    sLng = e.latlng['lng'];
+                    $("#value").val(sLat.toString() + "," + sLng.toString());
+                }
+                mymap.on('click', onMapClick);
+                $('#mymap').on('shown.bs.modal', function () {
+                    setTimeout(function () {
+                        mymap.invalidateSize();
+                    }, 1);
+                });
+            }
+            locationChoiceGenerate();
+        </script>
+        </div>
+    </div>
+    <label class="location-error">${label}</label>
+    
+
+
+    
+    `
+}
+
+var mdContentGenerate = function (message, kwargs) {
+    let converter = new showdown.Converter({
+        tables: true,
+        tablesHeaderId: "table"
+    });
     const htmlContents = converter.makeHtml(message);
     return `${htmlContents}`;
 }
 
-var multiChoiceGenerate = function(message, options, kwargs){
+var multiChoiceGenerate = function (message, options, kwargs) {
     let choices = ""
-    $.each(options, function(i, value){
+    $.each(options, function (i, value) {
         choices += `
         <div class="items col-xs-5 col-sm-5 col-md-3 col-lg-3">
             <div class="info-block block-info clearfix">
@@ -76,11 +144,11 @@ var multiChoiceGenerate = function(message, options, kwargs){
     return contents;
 }
 
-var singleChoiceGenerate = function(message, options, kwargs){
+var singleChoiceGenerate = function (message, options, kwargs) {
     let choices = "";
     const classes = ["primary", "success", "danger", "warning", "info"];
-    $.each(options, function(i, value){
-        if(i >= classes.length){
+    $.each(options, function (i, value) {
+        if (i >= classes.length) {
             i -= classes.length;
         }
         choices += `
@@ -95,9 +163,9 @@ var singleChoiceGenerate = function(message, options, kwargs){
     return contents;
 }
 
-var dropDownChoiceGenerate = function(message, options, kwargs){
+var dropDownChoiceGenerate = function (message, options, kwargs) {
     let choices = "";
-    $.each(options, function(i, value){
+    $.each(options, function (i, value) {
         choices += `<option value="${value}">${value}</option>`;
     });
     let contents = `
@@ -110,36 +178,38 @@ var dropDownChoiceGenerate = function(message, options, kwargs){
     return contents;
 }
 
-var addStep = function(reset){
-    if(reset) {
+var addStep = function (reset) {
+    if (reset) {
         $(".f1-steps").empty();
     }
     const currentStep = $(".f1-steps").children().length + 1;
-	const stepTemplate = `
+    const stepTemplate = `
 	<div class="f1-step active">
 		<div class="f1-step-icon">${currentStep}</div>
 	</div>`;
-	$(".f1-step:last-child").removeClass("active");
+    $(".f1-step:last-child").removeClass("active");
     $(".f1-steps").append(stepTemplate);
 }
 
-var generateSlide = function(res) {
+var generateSlide = function (res) {
     $("#spinner").toggle();
     // if error: leave the old slide and show the error
     if (res["error"]) {
         $("#error").html(res['error']);
         $(".btn-submit").attr("disabled", "false");
-        $(".form-box").toggle({"duration": 400});
+        $(".form-box").toggle({
+            "duration": 400
+        });
         return
     }
     // If the response contains redirect, so this was the final slide and will take new action
-    else if(res['cat'] === "redirect"){
-            $(location).attr("href", res["msg"]);
-            return
+    else if (res['cat'] === "redirect") {
+        $(location).attr("href", res["msg"]);
+        return
     }
     addStep(res['kwargs']['reset']);
     let contents = "";
-    switch(res['cat']){
+    switch (res['cat']) {
         case "string_ask":
             contents = stringContentGenerate(res['msg'], res['kwargs']);
             break;
@@ -151,6 +221,9 @@ var generateSlide = function(res) {
             break;
         case "int_ask":
             contents = intContentGenerate(res['msg'], res['kwargs']);
+            break;
+        case "captcha_ask":
+            contents = captchaContentGenerate(res['msg'], res['captcha'], res['label'], res['kwargs']);
             break;
         case "md_show":
             contents = mdContentGenerate(res['msg'], res['kwargs']);
@@ -164,8 +237,11 @@ var generateSlide = function(res) {
         case "drop_down_choice":
             contents = dropDownChoiceGenerate(res['msg'], res['options'], res['kwargs'])
             break;
+        case "location_ask":
+            contents = locationContentGenerate(res['msg'], res['options'], res['kwargs'])
+            break
     }
-	contents = `
+    contents = `
         <fieldset>
             <p id="error" class="red"></p>
 			${contents}
@@ -174,27 +250,29 @@ var generateSlide = function(res) {
 			</span>
 		</fieldset>`;
     $("#wizard").html(contents);
-    $(".form-box").toggle({"duration": 400});
+    $(".form-box").toggle({
+        "duration": 400
+    });
 
-	$(".btn-submit").on("click", function(ev){
+    $(".btn-submit").on("click", function (ev) {
         ev.preventDefault();
-		let value="";
-		if (["string_ask", "int_ask", "text_ask", "password_ask", "drop_down_choice"].includes(res['cat'])) {
-			value = $("#value").val();
-        } else if (res['cat'] === "single_choice"){
+        let value = "";
+        if (["string_ask", "int_ask", "text_ask", "password_ask", "drop_down_choice", "captcha_ask", "location_ask"].includes(res['cat'])) {
+            value = $("#value").val();
+        } else if (res['cat'] === "single_choice") {
             value = $("input[name='value']:checked").val();
-        } else if (res['cat'] === "multi_choice"){
+        } else if (res['cat'] === "multi_choice") {
             let values = [];
-            $("input[name='value[]']:checked").each( function () {
+            $("input[name='value[]']:checked").each(function () {
                 values.push($(this).val());
             });
             value = JSON.stringify(values);
         }
         // Validate the input
         const errors = validate(value, res['kwargs']['validate']);
-        if (errors.length > 0){
+        if (errors.length > 0) {
             var ul = $('<ul>');
-            $(errors).each(function(index, error) {
+            $(errors).each(function (index, error) {
                 ul.append($('<li>').html(error));
             });
             $("#error").html(ul);
@@ -204,30 +282,38 @@ var generateSlide = function(res) {
         $("#error").addClass("hidden");
         $(this).attr("disabled", "disabled");
         $("#spinner").toggle();
-        $(".form-box").toggle({"duration": 400});
+        $(".form-box").toggle({
+            "duration": 400
+        });
         EXEC_OBJ['command'] = "work_report";
         EXEC_OBJ['args']['result'] = value;
-		client.execute(EXEC_OBJ).then(function(res){
-		    EXEC_OBJ['command'] = "work_get";
-		    delete EXEC_OBJ['args']['result'];
-		    // Ignore work_report response and wait for getting next question
-		    client.execute(EXEC_OBJ).then(function(res){
+        client.execute(EXEC_OBJ).then(function (res) {
+            EXEC_OBJ['command'] = "work_get";
+            delete EXEC_OBJ['args']['result'];
+            // Ignore work_report response and wait for getting next question
+            client.execute(EXEC_OBJ).then(function (res) {
                 res = JSON.parse(res);
                 generateSlide(res);
             });
-		});
-	});
+        });
+    });
 }
+
+
 
 EXEC_OBJ["command"] = "session_new";
 // TOPIC is set through etlua template
-EXEC_OBJ["args"] = {"topic": TOPIC};
-client.execute(EXEC_OBJ).then(function(res){
+EXEC_OBJ["args"] = {
+    "topic": TOPIC
+};
+client.execute(EXEC_OBJ).then(function (res) {
     res = JSON.parse(res);
     EXEC_OBJ['command'] = "work_get";
-    EXEC_OBJ['args'] = {"sessionid": res['sessionid']}
-    client.execute(EXEC_OBJ).then(function(res){
+    EXEC_OBJ['args'] = {
+        "sessionid": res['sessionid']
+    }
+    client.execute(EXEC_OBJ).then(function (res) {
         res = JSON.parse(res);
-	    generateSlide(res);
-	});
+        generateSlide(res);
+    });
 });
