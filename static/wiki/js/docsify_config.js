eebbf63f904224e8d202dd567c08e2acdf6b01b9
samaradel <adels@greenitglobe.com>
2019-09-11 14:43:43 +0200

    Remove logo

--- static/wiki/js/docsify_config.js
+++ static/wiki/js/docsify_config.js
@@ -6,7 +6,6 @@ function docsifyConfig(name, repo) {
     TeamWidget.avatarPrefix = basePath;
 
     window.$docsify = {
-	logo: '../../static/img/tfn-small.png',
         coverpage: false,
         homepage: 'readme.md',
         basePath: basePath,
